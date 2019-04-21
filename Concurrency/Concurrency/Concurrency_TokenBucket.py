fb2b47e4e7784e5ec959e690758d1d3121ff5176
bluesilence <bluesilencemail@126.com>
2019-04-21 13:44:53 -0700

    Add queue in token bucket

--- Concurrency/Concurrency/Concurrency_TokenBucket.py
+++ Concurrency/Concurrency/Concurrency_TokenBucket.py
@@ -1,6 +1,7 @@
 import threading
 import time
 import random
+import heapq
 
 class TokenBucket(threading.Thread):
     def __init__(self, max_tokens, interval):
@@ -10,6 +11,7 @@ class TokenBucket(threading.Thread):
         self.lock = threading.Lock()
         self.max_tokens = max_tokens
         self.interval = interval
+        self.min_heap = [] # Process packats requesting fewer tokens first
 
     def run(self):
         while True:
@@ -18,26 +20,27 @@ class TokenBucket(threading.Thread):
                     self.tokens += 1
                     print('TokenBucket now has {} tokens...'.format(self.tokens))
 
+            self.process_requests()
+
             time.sleep(self.interval)
 
-    def get_tokens(self):
-        return self.tokens
-
-    def take_tokens(self, tokens_num):
+    def process_requests(self):
         with self.lock:
-            if tokens_num <= self.tokens:
-                self.tokens -= tokens_num
-                print('TokenBucket removed {} tokens, {} tokens left.'.format(tokens_num, self.tokens))
-                return True
-            else:
-                print('Required {} tokens but only has {}.'.format(tokens_num, self.tokens))
+            while self.min_heap and self.min_heap[0].size <= self.tokens:
+                packet = heapq.heappop(self.min_heap)
+                self.tokens -= packet.size
+                print('TokenBucket removed {} tokens for packet {}, {} tokens left.'.format(packet.size, packet.name, self.tokens))
+                packet.event.set()
 
-            return False
+    def add_requests(self, packet, tokens_num):
+        with self.lock: # heapq is not thread safe
+            heapq.heappush(self.min_heap, packet)
+            print('Packet {} requests for {} tokens, current requests: {}'.format(packet.name, tokens_num, [ p.size for p in self.min_heap ] ))
 
 class Packet(threading.Thread):
     id = 0
 
-    def __init__(self, size, arrive_time, token_bucket):
+    def __init__(self, size, arrive_time, token_bucket, event):
         threading.Thread.__init__(self)
 
         self.name = 'Packet-%d' % Packet.id
@@ -45,21 +48,27 @@ class Packet(threading.Thread):
         self.size = size
         self.arrive_time = arrive_time
         self.token_bucket = token_bucket
+        self.event = event
+
+    def __lt__(self, other):
+        return self.size < other.size or self.size == other.size and self.id < other.id
 
     def run(self):
-        time.sleep(self.arrive_time)
+        self.event.clear()
 
-        if self.token_bucket.take_tokens(self.size):
-            print('{} acquired {} tokens and was passed.'.format(self.name, self.size))
-        else:
-            print('{} did not get enough tokens and is non-conformant.'.format(self.name))
+        self.token_bucket.add_requests(self, self.size)
+
+        self.event.wait()
+        print('{} acquired {} tokens and was passed.'.format(self.name, self.size))
+
+        time.sleep(self.arrive_time)
 
 def main():
     N = 10
     max_tokens = 100
     interval = 1
     bucket = TokenBucket(max_tokens, interval)
-    packets = [ Packet(random.randint(1, max_tokens), random.randint(1, max_tokens * 3), bucket) for _ in range(N) ]
+    packets = [ Packet(random.randint(1, max_tokens // 10), random.randint(1, max_tokens * 3), bucket, threading.Event()) for _ in range(N) ]
 
     bucket.start()
     for packet in packets:
