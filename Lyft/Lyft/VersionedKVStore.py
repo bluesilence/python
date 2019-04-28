9bdaabc79cd801acd3c521f10e21017c3f0edbe7
bluesilence <bluesilencemail@126.com>
2019-04-27 20:56:52 -0700

    Add processLine

--- Lyft/Lyft/VersionedKVStore.py
+++ Lyft/Lyft/VersionedKVStore.py
@@ -49,6 +49,28 @@ class KVStore:
         # Get item in a balanced BST: O(logN)
         return tree[version]
 
+    def processLine(self, line):
+        tokens = line.upper().split(' ')
+        command = tokens[0]
+        if command == 'SET':
+            assert(len(tokens) == 3)
+
+            self.set(tokens[1], tokens[2])
+        elif command == 'UNSET':
+            assert(len(tokens) == 2)
+
+            return self.unset(tokens[1])
+        elif command == 'GET':
+            assert(len(tokens) == 2)
+
+            return self.get(tokens[1])
+        elif command == 'GETVERSION':
+            assert(len(tokens) == 3)
+
+            return self.getValueWithVersion(tokens[1], int(tokens[2]))
+        else:
+            raise NotImplementedError('Unsupported command: {}'.format(command))
+
 class TestKVStore(unittest.TestCase):
     def test_store(self):
         kvStore = KVStore()
@@ -66,8 +88,19 @@ class TestKVStore(unittest.TestCase):
         kvStore.set('c', 4)
         self.assertEqual(kvStore.get('c'), 4)
 
-        self.assertRaises(KeyError, kvStore.get, 'd')
-        self.assertRaises(KeyError, kvStore.getValueWithVersion, 'd', 1)
+    def test_lines(self):
+        kvStore = KVStore()
+
+        kvStore.processLine('SET a 1')
+        self.assertEqual(kvStore.processLine('GET a'), '1')
+        kvStore.processLine('SET b 2')
+        self.assertEqual(kvStore.processLine('GET b'), '2')
+        kvStore.processLine('UNSET b')
+        self.assertRaises(KeyError, kvStore.processLine, 'GET b')
+        self.assertRaises(ValueError, kvStore.processLine, 'GETVERSION a 1')
+        kvStore.processLine('SET a 3')
+        self.assertEqual(kvStore.processLine('GET a'), '3')
+        self.assertEqual(kvStore.processLine('GETVERSION a 1'), '3')
 
 if __name__ == '__main__':
     unittest.main()
